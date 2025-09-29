2150dbe6d50325722dbeb64499a94cb362cc86dd
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-09-29 07:41:34 -0300

    Resolvendo execicio 1

---
+++ untitled/src/Ex01.java
@@ -0,0 +1,11 @@
+public class Ex01 {
+    static void main() {
+        for(int i=7;i<=100;i++){
+            if(i%7==0){
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
