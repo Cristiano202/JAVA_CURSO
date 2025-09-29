daa3e53baa63d4f9a6205a17a05b27b8c8f68928
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-09-29 09:01:18 -0300

    Resolvendo execicio 3

---
+++ untitled/src/Ex03.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex03 {
+    public static boolean ehperfeito(int num){
+
+        int soma=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                soma+=i;
+            }
+        }
+        return soma==num;
+    }
+    static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Informe um numero: ");
+        int num=scanner.nextInt();
+        if(ehperfeito(num)){
+            System.out.println("É um numero perfeito");
+        }
+        else {
+            System.out.println("Não é um numero perfeito");
+        }
+
+
+    }
+
+
+}
