2a51cb63cd26355ac03dae7fa478a6bb3e58551a
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-18 19:07:10 -0300

    fazendo melhoria no software

--- untitled/src/Pratica10/TestCinema.java
+++ untitled/src/Pratica10/TestCinema.java
@@ -1,13 +1,18 @@
 package Pratica10;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TestCinema {
-    static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        CadrastroCinema cc=new CadrastroCinema();
-        boolean resposta=true;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<CadrastroCinema> listaFilmes = new ArrayList<>();
+
+        boolean resposta = true;
         do {
+            CadrastroCinema cc = new CadrastroCinema(); // novo filme a cada loop
+
             System.out.print("Informe o nome do filme: ");
             cc.setNome(sc.nextLine());
 
@@ -15,15 +20,23 @@ public class TestCinema {
             cc.setSala(sc.nextInt());
             sc.nextLine();
 
-            System.out.print("Informe o codigo do filme: ");
+            System.out.print("Informe o código do filme: ");
             cc.setCodigo(sc.nextInt());
             sc.nextLine();
 
-            System.out.print("Voce deseja continuar cadastrando filme: ");
-            resposta=sc.nextBoolean();
-            sc.nextLine();
-        }while (resposta==true);
+            listaFilmes.add(cc); // adiciona à lista
 
-        cc.imprimir();
+            System.out.print("Você deseja continuar cadastrando filmes? (true/false): ");
+            resposta = sc.nextBoolean();
+            sc.nextLine();
+        } while (resposta);
+
+        System.out.println("\nFilmes cadastrados:");
+        for (CadrastroCinema filme : listaFilmes) {
+            filme.imprimir();
+            System.out.println("----------------------");
+        }
+
+        sc.close();
     }
 }
