e5d939a710450ad5a334be90c00396149c790934
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-17 21:34:52 -0300

    praticando java

---
+++ untitled/src/Pratica10/CadrastroCinema.java
@@ -0,0 +1,34 @@
+package Pratica10;
+
+public class CadrastroCinema {
+    private String nome;
+    private int sala;
+    private int codigo;
+
+    public CadrastroCinema() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSala() {
+        return sala;
+    }
+
+    public void setSala(int sala) {
+        this.sala = sala;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
