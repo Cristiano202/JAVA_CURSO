4152cabf8f47bbd17240a0ffad0e9a62d99762e9
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-17 21:48:48 -0300

    fazendo melhoria no software

--- untitled/src/Pratica10/TestCinema.java
+++ untitled/src/Pratica10/TestCinema.java
@@ -6,8 +6,24 @@ public class TestCinema {
     static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         CadrastroCinema cc=new CadrastroCinema();
-        System.out.print("Informe o nome do filme: ");
-        cc.setNome(sc.nextLine());
+        boolean resposta=true;
+        do {
+            System.out.print("Informe o nome do filme: ");
+            cc.setNome(sc.nextLine());
+
+            System.out.print("Informe a sala: ");
+            cc.setSala(sc.nextInt());
+            sc.nextLine();
+
+            System.out.print("Informe o codigo do filme: ");
+            cc.setCodigo(sc.nextInt());
+            sc.nextLine();
+
+            System.out.print("Voce deseja continuar cadastrando filme: ");
+            resposta=sc.nextBoolean();
+            sc.nextLine();
+        }while (resposta==true);
+
         cc.imprimir();
     }
 }
