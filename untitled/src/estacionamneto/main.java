82e7e0960689c8f15c23cfb543c99427c5240aff
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-20 21:25:18 -0300

    fazendo melhoria no software

--- untitled/src/estacionamneto/main.java
+++ untitled/src/estacionamneto/main.java
@@ -5,6 +5,6 @@ import java.time.LocalDateTime;
 public class main {
     static void main(String[] args) {
         Moto moto =new Moto("1222","cris", "11","23");
-
+        moto.emprimir();
     }
 }
