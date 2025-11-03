386112753c835e054b57aa9134c0df695aecf5bc
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-11-03 08:02:36 -0300

    Emplementando inteface

--- untitled/src/AulaDia23/Aula001.java
+++ untitled/src/AulaDia23/Aula001.java
@@ -1,11 +1,23 @@
 package AulaDia23;
 
+import java.util.Scanner;
+
 public class Aula001 {
     static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         Aviao aviao=new Aviao();
         Passaro passaro=new Passaro();
         System.out.println("Aula sobre interfaces");
+
+        System.out.print("Qual modelo do aviao: ");
+        aviao.setModelo(sc.nextLine());
+        System.out.print("Qual ano do aviao: ");
+        aviao.setAno(sc.nextInt());
+        sc.nextLine();
+
         aviao.voar();
+        System.out.println("Qual especie do passaro: ");
+        passaro.setEspecie(sc.nextLine());
         passaro.voar();
 
     }
--- untitled/src/AulaDia23/Passaro.java
+++ untitled/src/AulaDia23/Passaro.java
@@ -1,6 +1,15 @@
 package AulaDia23;
 
 public class Passaro implements Voavel{
+    private String especie;
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
 
     @Override
     public void voar() {
